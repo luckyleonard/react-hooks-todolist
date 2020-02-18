2e422f107ab43e6231c8c078581a2ab16760b30f
luckyleonard <dzhu31@hotmail.com>
2020-02-18 23:42:32 +1100

    Updates

---
+++ precache-manifest.be65d0ddf882b0f37db92fbf909585db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f104f3666db434e92768eb93fc050d42",
+    "url": "/react-hooks-todolist/index.html"
+  },
+  {
+    "revision": "862a3ae7b269fdca8014",
+    "url": "/react-hooks-todolist/static/css/main.45bc6b2b.chunk.css"
+  },
+  {
+    "revision": "17c234cb73ac20099c06",
+    "url": "/react-hooks-todolist/static/js/2.1bd0b94d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-hooks-todolist/static/js/2.1bd0b94d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "862a3ae7b269fdca8014",
+    "url": "/react-hooks-todolist/static/js/main.feb80932.chunk.js"
+  },
+  {
+    "revision": "ee3e60011dfb79db92ac",
+    "url": "/react-hooks-todolist/static/js/runtime-main.bc4b6582.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-hooks-todolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
